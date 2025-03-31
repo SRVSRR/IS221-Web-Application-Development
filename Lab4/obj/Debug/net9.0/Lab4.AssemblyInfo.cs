576397b77a30e598abd7571988c0b831945a2d3a
Rohan Nandan <rohannandan2004@gmail.com>
2025-03-31 17:30:44 +1200

    Feat: Labs Wk 5

--- Lab4/obj/Debug/net9.0/Lab4.AssemblyInfo.cs
+++ Lab4/obj/Debug/net9.0/Lab4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+650cd071d80f8a48174f7051e84c57cb958911ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecb318ea31b4fe55cefbd5693cc43165ab788a01")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

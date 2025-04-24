f4d66c17c43c73bd1bbd6e96e2aa45dcaeb29279
Rohan Nandan <rohannandan2004@gmail.com>
2025-04-25 03:48:47 +1200

    Feat: Learning tables

--- Lab4/obj/Debug/net9.0/Lab4.AssemblyInfo.cs
+++ Lab4/obj/Debug/net9.0/Lab4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7054a0dcd6ce68e84a3acadd0aca084204de4f0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+beca66feafaed736d996fa5b60f5bc0f5e023fc8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
